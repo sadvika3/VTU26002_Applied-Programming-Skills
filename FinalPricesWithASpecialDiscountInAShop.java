6c8fee8f2bde4904ba3e087abd97da787fd6f405
sadvika3 <sadvika61@gmail.com>
2026-03-01 11:37:04 +0530

    Create FinalPricesWithASpecialDiscountInAShop.java

---
+++ FinalPricesWithASpecialDiscountInAShop.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                int idx = stack.pop();
+                result[idx] = prices[idx] - prices[i];
+            }
+            stack.push(i);
+            result[i] = prices[i];
+        }
+
+        return result;
+    }
+}
