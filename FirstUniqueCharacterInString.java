f79aee694c27d92e9b1e2445047fb87515c9446a
sadvika3 <sadvika61@gmail.com>
2026-03-01 11:09:43 +0530

    Update FirstUniqueCharacterInString.java

--- FirstUniqueCharacterInString.java
+++ FirstUniqueCharacterInString.java
@@ -1,8 +1,6 @@
 class Solution {
     public int firstUniqChar(String s) {
         int[] count = new int[26];
-
-        // Count frequency of each character
         for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
         }
